1e259c3299594609faab899d24862deded9267b3
oumartom <oumartom45@gmail.com>
2025-06-11 15:48:23 +0100

    pour ajouter superuser

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
-
-# 1. Installer les dépendances
 pip install -r requirements.txt
-
-# 2. Appliquer les migrations de base de données
 python manage.py migrate
-
-# 3. Collecter les fichiers statiques
 python manage.py collectstatic --noinput
-
-# 4. Autres commandes personnalisées si nécessaire
-# python manage.py loaddata initial_data.json
+python manage.py loaddata initial_data.json
+python manage.py shell < create_superuser.py
